4e003c77c0cbcfbd73a2e66c94c0738b3d3583fa
lidongjie2 <lidongjie2@yy.com>
2024-05-28 16:12:49 +0800

    推迟阅读状态优化

--- app/src/main/java/com/example/composeoptimization/compose/DeferReadingStatePage.kt
+++ app/src/main/java/com/example/composeoptimization/compose/DeferReadingStatePage.kt
@@ -1,25 +1,23 @@
 package com.example.composeoptimization.compose
 
+import androidx.compose.animation.core.FastOutLinearInEasing
+import androidx.compose.animation.core.RepeatMode
+import androidx.compose.animation.core.VectorConverter
+import androidx.compose.animation.core.animateValue
+import androidx.compose.animation.core.infiniteRepeatable
+import androidx.compose.animation.core.rememberInfiniteTransition
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.size
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.State
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.produceState
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
-import androidx.compose.runtime.withFrameMillis
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.geometry.Size
-import androidx.compose.ui.graphics.painter.Painter
-import androidx.compose.ui.layout.onPlaced
-import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.IntOffset
-import androidx.compose.ui.unit.IntSize
 import androidx.compose.ui.unit.dp
 import com.example.composeoptimization.R
 
@@ -32,80 +30,54 @@ import com.example.composeoptimization.R
 
 @Composable
 fun DeferReadingStatePage() {
-    val logo = painterResource(id = R.drawable.compose_logo)
-    var size by remember { mutableStateOf(IntSize.Zero) }
-    val logoPosition by logoPosition(size = size, logoSize = logo.intrinsicSize)
+    val offset by offsetAnimate()
+    Box(modifier = Modifier.fillMaxSize()) {
+        NiceGridUI()
+        ImageAnimLambda({ offset })
+    }
+}
 
-    Box(
+@Composable
+private fun ImageAnim(offset: IntOffset) {
+    Image(
+        painter = painterResource(id = R.drawable.compose_logo), contentDescription = "logo",
         modifier = Modifier
-            .fillMaxSize()
-            .onPlaced {
-                size = it.size
-            }
-    ) {
-        with(LocalDensity.current) {
-            ImageItem1(logo, logoPosition)
-//            ImageItem2(logo, { logoPosition })
-//            Image(
-//                painter = logo,
-//                contentDescription = "logo",
-//                modifier = Modifier.offset { logoPosition }
-//            )
-        }
-    }
-}
-
-@Composable
-fun ImageItem(logo: Painter, offset: () -> IntOffset) {
-    Image(
-        painter = logo,
-        contentDescription = "logo",
-        modifier = Modifier.offset { offset() }
+            .size(80.dp)
+            .offset(x = offset.x.dp, y = offset.y.dp)
     )
 }
 
 @Composable
-fun ImageItem1(logo: Painter, offset: IntOffset) {
+private fun NiceGridUI() {
+    Box(modifier = Modifier.fillMaxSize())
+}
+
+@Composable
+private fun offsetAnimate(): State<IntOffset> = rememberInfiniteTransition().animateValue(
+    initialValue = IntOffset(0, 0), targetValue = IntOffset(200, 400), typeConverter = IntOffset.VectorConverter,
+    animationSpec = infiniteRepeatable(
+        repeatMode = RepeatMode.Reverse,
+        animation = tween<IntOffset>(
+            durationMillis = 2000,
+            easing = FastOutLinearInEasing,
+        ),
+    ), label = ""
+)
+
+@Composable
+private fun ImageAnimDefer(offset: () -> IntOffset) {
     Image(
-        painter = logo,
-        contentDescription = "logo",
-        modifier = Modifier.offset { { offset }() }
+        painter = painterResource(id = R.drawable.compose_logo), contentDescription = "logo",
+        modifier = Modifier
+            .size(80.dp)
+            .offset(x = offset().x.dp, y = offset().y.dp)
     )
 }
 
 @Composable
-fun ImageItem2(logo: Painter, offset: () -> IntOffset) {
-    Image(
-        painter = logo,
-        contentDescription = "logo",
-        modifier = Modifier.offset(x = offset().x.dp, y = offset().y.dp)
-    )
+private fun ImageAnimLambda(offset: () -> IntOffset) {
+    Image(painter = painterResource(id = R.drawable.compose_logo), contentDescription = "logo",
+        modifier = Modifier
+            .size(80.dp)
+            .offset { offset() })
 }
-
-@Composable
-fun logoPosition(size: IntSize, logoSize: Size): State<IntOffset> =
-    produceState(initialValue = IntOffset.Zero, size, logoSize) {
-        if (size == IntSize.Zero) {
-            this.value = IntOffset.Zero
-            return@produceState
-        }
-
-        var xDirection = 1
-        var yDirection = 1
-
-        while (true) {
-            withFrameMillis {
-                value += IntOffset(x = MOVE_SPEED * xDirection, y = MOVE_SPEED * yDirection)
-
-                if (value.x <= 0 || value.x >= size.width - logoSize.width) {
-                    xDirection *= -1
-                }
-
-                if (value.y <= 0 || value.y >= size.height - logoSize.height) {
-                    yDirection *= -1
-                }
-            }
-        }
-    }
-
-internal const val MOVE_SPEED = 10
